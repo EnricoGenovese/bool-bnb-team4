624ba220cbf232a7a772a97c871b2023132bd1b9
Danilo-Mosca <danilo.mosca85@gmail.com>
2025-02-13 17:05:29 +0100

    fix SingleApartment

---
+++ frontend/src/components/ReviewForm.jsx
@@ -0,0 +1,10 @@
+import { useState } from "react";
+import { useGlobalContext } from "../contexts/GlobalContext";
+
+function ReviewForm() {
+    return (
+        <h2>Prova</h2>
+    );
+}
+
+export default ReviewForm;
