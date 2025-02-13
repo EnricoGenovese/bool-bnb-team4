6bbe4b2317c20d5ada9407860df1ab83f4b14884
EnricoGeno <enricogeno@libero.it>
2025-02-13 12:16:41 +0100

    global layout: homepage;

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,8 +1,9 @@
 
 
-
 export default function Footer() {
     return (
-        <h1>Footer</h1>
+        <div className="container bg-dark text-white text-center">
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Magni eveniet voluptate recusandae? Consequatur obcaecati consequuntur, deleniti officiis incidunt eum fuga beatae iste! Minus earum provident error harum itaque reprehenderit fuga!</p>
+        </div>
     )
 }
--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,8 +1,13 @@
-
-
+import { NavLink } from "react-router";
 
 export default function Navbar() {
     return (
-        <p>Navbar</p>
+        <nav className="navbar sticky-top navbar-dark bg-dark mb-5">
+            <div className="d-flex justify-content-between align-items-center px-3">
+                <NavLink to={`/`} className="navbar-brand" href="#">Homepage</NavLink>
+                <NavLink to={`/advanced-research`} className="navbar-brand" href="#">Search an apartment</NavLink>
+                <NavLink to={`/post-apartment`} className="navbar-brand" href="#">Post your apartment</NavLink>
+            </div>
+        </nav>
     )
 }
