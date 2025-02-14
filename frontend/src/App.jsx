f087f40304c9ac615101433ea481134b757e92d4
DavidIncarbone <david.incarbone@gmail.com>
2025-02-14 12:59:58 +0100

    fixed alert

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,6 +7,7 @@ import ApartmentDetails from "./pages/ApartmentDetails.jsx";
 import ApartmentPostForm from "./pages/ApartmentPostForm.jsx";
 import Homepage from "./pages/Homepage.jsx";
 import NotFound from "./pages/NotFound.jsx";
+import ContactForm from "./pages/ContactForm.jsx";
 
 
 
